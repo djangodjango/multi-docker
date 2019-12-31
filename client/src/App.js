922912dd199a01b24731d39aa7368e3e83e67483
Melvin Effilab <melvin@Mackbook-Pro-Django.local>
2019-12-31 12:02:06 +0100

    added directory

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1 className="App-Title"> Welcome to Fibonacci react APP</h1>
+          <p>
+            <Link to="/">Home</Link>
+          </p>
+          <p>
+            <Link to="/otherpage">Other Page</Link>
+          </p>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
